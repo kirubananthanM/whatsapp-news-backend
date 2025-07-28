91914b8b0c68f60f44740d2c6ccd890157eacc63
kirubananthanM <kirubananthan1155@gmail.com>
2025-07-28 18:51:34 +0530

    Use .env file for secrets

--- backend_utils.py
+++ backend_utils.py
@@ -1,9 +1,13 @@
 import requests
 from twilio.rest import Client
+from dotenv import load_dotenv
+import os
 
-TWILIO_SID = "ACbae10f24e7efe3ce8e176be07bb3d450"
-TWILIO_AUTH_TOKEN = "61557ff372d44dd738799828392866eb"
-FROM_WHATSAPP_NUMBER = "whatsapp:+14155238886"
+load_dotenv()
+
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+FROM_WHATSAPP_NUMBER = os.getenv("FROM_WHATSAPP_NUMBER")
 client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
 def get_latest_news(topic):
--- twilio_utils.py
+++ twilio_utils.py
@@ -1,14 +1,17 @@
 from twilio.rest import Client
+from dotenv import load_dotenv
+import os
 
-TWILIO_SID = "ACbae10f24e7efe3ce8e176be07bb3d450"
-AUTH_TOKEN = "61557ff372d44dd738799828392866eb"
-FROM_NUMBER = "whatsapp:+14155238886"
+load_dotenv()
 
-client = Client(TWILIO_SID, AUTH_TOKEN)
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+FROM_WHATSAPP_NUMBER = os.getenv("FROM_WHATSAPP_NUMBER")
+client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
 
 def send_whatsapp(to_number, message):
     client.messages.create(
-        from_=FROM_NUMBER,
+        from_=FROM_WHATSAPP_NUMBER,
         to=f"whatsapp:{to_number}",
         body=message
     )
